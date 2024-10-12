14de563778659e726aa4afaa27f4d274fd9a89cd
Nasif Fuad <nasifuad007@gmail.com>
2024-10-12 08:50:21 +0600

    Done

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const api =
-  "http://api.weatherapi.com/v1/current.json?key=648052733b4b41ec86e15742241210&q=Dhaka";
-
+  "http://api.weatherapi.com/v1/current.json?key=648052733b4b41ec86e15742241210&q=";
+const search = document.getElementById("search");
+const input = document.getElementById("input");
+const name = document.getElementById("name");
+const temp = document.getElementById("temp");
+const weather = document.getElementById("weather");
+const hi_low = document.getElementById("hi-low");
 async function see_weather(url) {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
-  // return data;
+  return data;
 }
-see_weather(api);
+async function show_weather(url) {
+  const data = await see_weather(url);
+  name.innerText = data.location.name;
+  temp.innerText = data.current.temp_c;
+  weather.innerText = data.current.condition.text;
+  hi_low.innerText = `${data.current.temp_c}°C / ${data.current.temp_f}°F`;
+}
+search.addEventListener("click", () => {
+  const new_api = `${api}${input.value}`;
+  show_weather(new_api);
+});
