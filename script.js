24f48101b8f6b242c9cdfa890d5a3f471a23f7a3
Nasif Fuad <nasifuad007@gmail.com>
2024-10-23 20:38:15 +0600

    default image according to weather added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const city = document.getElementById("name");
 const temp = document.getElementById("temp");
 const humidity = document.getElementById("humidity");
 const wind = document.getElementById("wind");
+const image = document.getElementById("image");
 async function see_weather(url) {
   const response = await fetch(url);
   const data = await response.json();
@@ -22,6 +23,16 @@ async function show_weather(url) {
   console.log(data.current.wind_kph);
   wind.innerText = data.current.wind_kph + "km/h";
   humidity.innerText = data.current.humidity + "%";
+
+  if (data.current.temp_c < 1) {
+    image.src = "./images/snow.png";
+  } else if (data.current.temp_c > 0 && data.current.temp_c < 10) {
+    image.src = "./images/rain.png";
+  } else if (data.current.temp_c > 10 && data.current.temp_c < 20) {
+    image.src = "./images/clouds.png";
+  } else {
+    image.src = "./images/clear.png";
+  }
 }
 search.addEventListener("click", () => {
   const new_api = `${api}${input.value}`;
