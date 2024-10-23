d0132cd0ce1f3726021af18db71b5877e94b98b4
Nasif Fuad <nasifuad007@gmail.com>
2024-10-23 20:33:58 +0600

    Updated

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const api =
   "https://api.weatherapi.com/v1/current.json?key=648052733b4b41ec86e15742241210&q=";
 const search = document.getElementById("search");
 const input = document.getElementById("input");
-const name = document.getElementById("name");
+const city = document.getElementById("name");
 const temp = document.getElementById("temp");
-const weather = document.getElementById("weather");
-const hi_low = document.getElementById("hi-low");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
 async function see_weather(url) {
   const response = await fetch(url);
   const data = await response.json();
@@ -13,12 +13,15 @@ async function see_weather(url) {
   console.log(data.current.temp_c);
   return data;
 }
+
 async function show_weather(url) {
   const data = await see_weather(url);
-  name.innerText = data.location.name;
-  temp.innerText = data.current.temp_c;
-  weather.innerText = data.current.condition.text;
-  hi_low.innerText = `${data.current.temp_c}°C / ${data.current.temp_f}°F`;
+  console.log(data);
+  city.innerText = data.location.name;
+  temp.innerText = data.current.temp_c + "°C";
+  console.log(data.current.wind_kph);
+  wind.innerText = data.current.wind_kph + "km/h";
+  humidity.innerText = data.current.humidity + "%";
 }
 search.addEventListener("click", () => {
   const new_api = `${api}${input.value}`;
@@ -30,7 +33,7 @@ addEventListener("keyup", (e) => {
     show_weather(new_api);
   }
 });
-input.addEventListener("keydown", (e) => {
-  const new_api = `${api}${input.value}`;
-  show_weather(new_api);
-});
+// input.addEventListener("keydown", (e) => {
+//   const new_api = `${api}${input.value}`;
+//   show_weather(new_api);
+// });
